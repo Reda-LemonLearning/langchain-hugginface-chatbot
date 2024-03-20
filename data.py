423464aeeb08cee8f0b69e0622f1a30be6c489d8
Reda Sarehane <reda.sarehane@lemonlearning.fr>
2024-03-20 11:16:09 +0100

    added data.py file used to retreive and split dataset into managable chunks for embedding

---
+++ data.py
@@ -0,0 +1,71 @@
+import os 
+from dotenv import load_dotenv
+import pandas as pd
+from typing import Optional, List, Tuple
+from datasets import Dataset
+import matplotlib.pyplot as plt
+import datasets
+from langchain.docstore.document import Document as LangchainDocument
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from transformers import AutoTokenizer
+
+load_dotenv()
+
+pd.set_option("display.max_colwidth", None)  # helpful when visualizing retriever outputs
+
+ds = datasets.load_dataset("m-ric/huggingface_doc", split="train")
+
+RAW_KNOWLEDGE_BASE = [
+LangchainDocument(page_content=doc["text"], metadata={"source": doc["source"]}) for doc in ds
+]
+
+MARKDOWN_SEPARATORS = [
+    "\n#{1,6} ",
+    "```\n",
+    "\n\\*\\*\\*+\n",
+    "\n---+\n",
+    "\n___+\n",
+    "\n\n",
+    "\n",
+    " ",
+    "",
+]
+
+EMBEDDING_MODEL_NAME = os.getenv("EMBEDDING_MODEL_ID")
+
+def split_documents(
+    chunk_size: int,
+    knowledge_base: List[LangchainDocument],
+    tokenizer_name: Optional[str] = EMBEDDING_MODEL_NAME,
+) -> List[LangchainDocument]:
+    """
+    Split documents into chunks of maximum size `chunk_size` tokens and return a list of documents.
+    """
+    text_splitter = RecursiveCharacterTextSplitter.from_huggingface_tokenizer(
+        AutoTokenizer.from_pretrained(tokenizer_name),
+        chunk_size=chunk_size,
+        chunk_overlap=int(chunk_size / 10),
+        add_start_index=True,
+        strip_whitespace=True,
+        separators=MARKDOWN_SEPARATORS,
+    ) 
+
+    docs_processed = []
+    for doc in knowledge_base:
+        docs_processed += text_splitter.split_documents([doc])
+
+    # Remove duplicates
+    unique_texts = {}
+    docs_processed_unique = []
+    for doc in docs_processed:
+        if doc.page_content not in unique_texts:
+            unique_texts[doc.page_content] = True
+            docs_processed_unique.append(doc)
+
+    return docs_processed_unique
+
+docs_processed = split_documents(
+    512,  # We choose a chunk size adapted to our model
+    RAW_KNOWLEDGE_BASE,
+    tokenizer_name=EMBEDDING_MODEL_NAME,
+)
--- main.py
+++ main.py
@@ -3,13 +3,13 @@ from dotenv import load_dotenv
 from langchain_community.llms import HuggingFaceEndpoint
 from langchain.chains import LLMChain
 from langchain.prompts import PromptTemplate
-from huggingface_hub import login
+from langchain.schema import HumanMessage, SystemMessage
+from langchain_community.chat_models.huggingface import ChatHuggingFace
 
 load_dotenv()
 
 HUGGINGFACEHUB_API_TOKEN = os.getenv('HUGGINGFACEHUB_API_TOKEN')
-
-repo_id = "mistralai/Mistral-7B-Instruct-v0.2"
+MODEL_REPO_ID = os.getenv("MODEL_REPO_ID")
 
 question = "Who is the 2008 ballon d'or winner ?"
 template = """Question: {question}
@@ -18,7 +18,7 @@ Answer : Let's think step by step.
 prompt = PromptTemplate.from_template(template)
 
 llm = HuggingFaceEndpoint(
-    repo_id=repo_id, max_length=128, temperature=0.5, token=HUGGINGFACEHUB_API_TOKEN
+    repo_id=MODEL_REPO_ID, max_length=128, temperature=0.5, token=HUGGINGFACEHUB_API_TOKEN
 )
 
 llm_chain = LLMChain(prompt=prompt, llm=llm)
